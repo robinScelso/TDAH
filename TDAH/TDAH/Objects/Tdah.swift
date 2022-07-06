7d29632328472d8a0f61827842a810bccf217ac3
Robin scelso <46759103+robinScelso@users.noreply.github.com>
2022-07-06 21:57:24 +0200

    Create application architecture

---
+++ TDAH/TDAH/Objects/Tdah.swift
@@ -0,0 +1,14 @@
+//
+//  Tdah.swift
+//  TDAH
+//
+//  Created by Robin SCELSO on 15/03/2022.
+//
+
+import Foundation
+
+public class Tdah{
+    let _id = 1
+    var symptom: [String] = []
+}
+
---
+++ TDAH/TDAH/TDAHApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDAHApp.swift
+//  TDAH
+//
+//  Created by Robin SCELSO on 15/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct TDAHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
