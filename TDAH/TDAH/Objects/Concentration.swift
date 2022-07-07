c600b0b5ffd4efea63a75c23ba86aec741a2064f
Robin scelso <46759103+robinScelso@users.noreply.github.com>
2022-07-07 09:41:48 +0200

    Adding Object Concentration

---
+++ TDAH/TDAH/Objects/Concentration.swift
@@ -0,0 +1,13 @@
+//
+//  Concentration.swift
+//  TDAH
+//
+//  Created by Robin SCELSO on 07/07/2022.
+//
+
+import Foundation
+
+class Concentration: ObservableObject {
+    var hour: Int = 0
+    var minute: Int = 0
+}
