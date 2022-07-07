b8aab9781b5e9269365a3f43a49b0beec9e4c8d3
Robin scelso <46759103+robinScelso@users.noreply.github.com>
2022-07-07 14:57:48 +0200

    Adding Firebase and kingfisher package

--- TDAH/TDAH/Views/ContentView.swift
+++ TDAH/TDAH/Views/ContentView.swift
@@ -9,29 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView{
-            ZStack(alignment: Alignment(horizontal: .trailing, vertical: .bottom)){
-                ScrollView{
-                    VStack{
-                        //TODO foreach with activities and concentration time
-                        
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 16, style:
-                                                /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/)
-                            .fill(Color.white)
-                            .shadow(color: Color(Color .RGBColorSpace.sRGB, white: 0,
-                             opacity: 0.2), radius: 4)
-                        }
-                    }
-                    .padding(.horizontal, 16)
-                    
-                }
-                .navigationTitle("My TDAH")
-                
-                
-                
-            }
-                
+        TabView{
+            HomeView()
         }
     }
 }
---
+++ TDAH/TDAH/Views/Home/HomeView.swift
@@ -0,0 +1,28 @@
+//
+//  HomeView.swift
+//  TDAH
+//
+//  Created by Robin SCELSO on 07/07/2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView{
+            ZStack(alignment: Alignment(horizontal: .trailing, vertical: .bottom)){
+                ScrollView{
+                    VStack{
+                        
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
